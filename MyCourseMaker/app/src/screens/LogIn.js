16d0e35a09195446bac6e1ae02ad5fede2e5733e
Arnulfo Santillan Jr <59852318+arnulfosjr@users.noreply.github.com>
2025-04-26 11:46:32 -0700

    added index and login file pages

---
+++ MyCourseMaker/app/src/screens/LogIn.js
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function LogIn() {
+  return (
+    <View
+        style = {{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+    >
+        <Text></Text>
+    </View>
+    );
+
+}
